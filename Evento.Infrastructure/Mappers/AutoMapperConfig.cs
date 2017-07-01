c687276f2d4a7424883b6d0e303866d2ad437ebb
Tomasz Krzywicki <t-krzywicki@wp.pl>
2017-07-01 22:14:48 +0200

    mapping to DTO and automapper has been added

---
+++ Evento.Infrastructure/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using Evento.Core.Domain;
+using Evento.Infrastructure.DTO;
+
+namespace Evento.Infrastructure.Mappers
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Initialize()
+            => new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Event, EventDto>()
+                .ForMember(x => x.TicketsCount, m => m.MapFrom(p => p.Tickets.Count()));
+            })
+            .CreateMapper();
+    }
+}
